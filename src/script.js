5966601b48106bb7d456cd8fdaa6c2f9d45fa48f
Rebecca86 <kaizens.rebecca@gmail.com>
2025-02-16 15:18:55 +0800

    API Integration for the poem

--- src/script.js
+++ src/script.js
@@ -1,13 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayAnimatedPoem(response) {
   new Typewriter("#poem-content", {
-    strings: `My love for you, like a river, flows
-My heart it swells, as I’m exposed
-To your beauty, like a star, aglow
-Your presence, like a melody, I know`,
+    strings: response.data.answer,
     autoStart: true,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let character = document.querySelector("#search-input");
+  //   alert(character);
+  let apiKey = "f300cf6ad21c42dc4f0oefe03b237t4a";
+  let context =
+    "Please search for a romantic poem and please keep the length in one sentence.";
+  let prompt = `Please search for a poem describing the character ${character}?`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let divPoemElement = document.querySelector("#poem-content");
+  divPoemElement.innerHTML =
+    "The AI is searching for a romantic poem that will hyptonize you....";
+  axios.get(apiUrl).then(displayAnimatedPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-search-form");
